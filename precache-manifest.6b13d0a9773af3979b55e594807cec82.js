08aa700e9481950ec7011270790aebc562dc56c5
William Bruntrager <w.bruntrager@gmail.com>
2020-09-16 14:21:19 +0200

    Updates

---
+++ precache-manifest.6b13d0a9773af3979b55e594807cec82.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "976c612283588c55e3b5949d42497a24",
+    "url": "/index.html"
+  },
+  {
+    "revision": "338a68f0ed41d00b8059",
+    "url": "/static/css/main.cfd6f708.chunk.css"
+  },
+  {
+    "revision": "b8ea8ec98ee8a999606d",
+    "url": "/static/js/2.99b11b20.chunk.js"
+  },
+  {
+    "revision": "70398313a0ec7fecf519184b0e1f7817",
+    "url": "/static/js/2.99b11b20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "338a68f0ed41d00b8059",
+    "url": "/static/js/main.dadcc440.chunk.js"
+  },
+  {
+    "revision": "0b453c4eb30576e51974",
+    "url": "/static/js/runtime-main.c635c86c.js"
+  },
+  {
+    "revision": "ed402502df852ff5a111b1d6a520d30f",
+    "url": "/static/media/Fascist.ed402502.JPG"
+  },
+  {
+    "revision": "5179a2ef7a522fb5971c6e59645a7ece",
+    "url": "/static/media/Liberal.5179a2ef.JPG"
+  },
+  {
+    "revision": "ed402502df852ff5a111b1d6a520d30f",
+    "url": "/static/media/fascist-card-enact.ed402502.JPG"
+  },
+  {
+    "revision": "61fe29afccc13d3216d2ae3f8ac39dcd",
+    "url": "/static/media/fascist-card-space.61fe29af.JPG"
+  },
+  {
+    "revision": "84f89324da4135662a9186f36227c960",
+    "url": "/static/media/fascist-card-win.84f89324.JPG"
+  },
+  {
+    "revision": "5179a2ef7a522fb5971c6e59645a7ece",
+    "url": "/static/media/liberal-card-enact.5179a2ef.JPG"
+  },
+  {
+    "revision": "f5c7314a7a80f6c447ffdda37e8a759a",
+    "url": "/static/media/liberal-card-space.f5c7314a.JPG"
+  },
+  {
+    "revision": "de286c5f109e5e9b521df5e4978db2ba",
+    "url": "/static/media/liberal-card-win.de286c5f.JPG"
+  }
+]);
